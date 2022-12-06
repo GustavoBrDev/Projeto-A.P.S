abb0272049a489e5373dc212a8de47745b680801
kelvin <aluno@172.16.3.54>
2022-12-06 16:20:47 -0300

    criação da classe fatura, faltou terminar imprimirfatura  .

---
+++ Projeto A.P.S/src/com/ifsc/tds/Fatura.java
@@ -0,0 +1,63 @@
+package com.ifsc.tds;
+
+public class Fatura {
+	
+	private int faturald;
+	
+	private int MesAno;
+	
+	private UC unidadeConsumidora;
+	
+	private int consumo;
+	
+	private boolean pago;
+
+	public int getFaturald() {
+		return faturald;
+	}
+
+	public void setFaturald(int faturald) {
+		this.faturald = faturald;
+	}
+
+	public int getMesAno() {
+		return MesAno;
+	}
+
+	public void setMesAno(int mesAno) {
+		MesAno = mesAno;
+	}
+
+	public UC getUnidadeConsumidora() {
+		return unidadeConsumidora;
+	}
+
+	public void setUnidadeConsumidora(UC unidadeConsumidora) {
+		this.unidadeConsumidora = unidadeConsumidora;
+	}
+
+	public int getConsumo() {
+		return consumo;
+	}
+
+	public void setConsumo(int consumo) {
+		this.consumo = consumo;
+	}
+
+	public boolean isPago() {
+		return pago;
+	}
+
+	public void setPago(boolean pago) {
+		this.pago = pago;
+	}
+	
+	public double calcularValorFatura () {
+		return 0;
+	}
+	public String toString () {
+		return "";
+	}
+	
+
+}
