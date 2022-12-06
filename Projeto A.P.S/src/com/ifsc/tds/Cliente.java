7924a181251131da855645e9d22d14e308c90716
kelvin <aluno@172.16.3.54>
2022-12-06 15:53:29 -0300

    eu fiz muito

--- Projeto A.P.S/src/com/ifsc/tds/Cliente.java
+++ Projeto A.P.S/src/com/ifsc/tds/Cliente.java
@@ -1,5 +1,38 @@
 package com.ifsc.tds;
 
-public class Cliente {
+public class Cliente  {
+	
+	private int clienteld;
+	
+	private String nome;
+	
+	private Endereco endereco;
+	
+	
 
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getClienteld() {
+		return clienteld;
+	}
+
+	public void setClienteld(int clienteld) {
+		this.clienteld = clienteld;
+	}
+	
+	
 }
