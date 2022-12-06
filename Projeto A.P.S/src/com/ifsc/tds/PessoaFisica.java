edb2a049d625428b04c11f8df4f4e8090850b58b
Gustavo <aluno@172.16.3.29>
2022-12-06 15:54:27 -0300

    Atribuição de atributos e definição dos respctivos métodos e métodos construtor

--- Projeto A.P.S/src/com/ifsc/tds/PessoaFisica.java
+++ Projeto A.P.S/src/com/ifsc/tds/PessoaFisica.java
@@ -1,5 +1,23 @@
 package com.ifsc.tds;
 
 public class PessoaFisica extends Cliente {
+	
+	private String cpf;
+	
+	public PessoaFisica () {
+		
+	}
 
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String toString () {
+		return "Não sei o que é isso";
+	}
+	
 }
--- Projeto A.P.S/src/com/ifsc/tds/PessoaJuridica.java
+++ Projeto A.P.S/src/com/ifsc/tds/PessoaJuridica.java
@@ -2,4 +2,18 @@ package com.ifsc.tds;
 
 public class PessoaJuridica extends Cliente {
 
+	private String cpnj;
+
+	public String getCpnj() {
+		return cpnj;
+	}
+
+	public void setCpnj(String cpnj) {
+		this.cpnj = cpnj;
+	}
+	
+	public String toString() {
+		return "";
+	}
+	
 }
